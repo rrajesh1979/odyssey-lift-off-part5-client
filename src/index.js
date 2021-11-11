2ed798680630cbda50c22007d33e90334ac716eb
Rajesh Rajagopalan <rrajesh1979@gmail.com>
2021-11-10 21:06:29 -0500

    Updated server URL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://rrajesh1979-catstronauts-serv.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
